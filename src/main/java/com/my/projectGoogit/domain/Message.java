3be4521a242cf2314fac5477699d0d36b3c259ac
willy950205 <willy950205@gmail.com>
2020-11-22 16:55:14 +0900

    domain 수정

--- src/main/java/com/my/projectGoogit/domain/Message.java
+++ src/main/java/com/my/projectGoogit/domain/Message.java
@@ -8,6 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Temporal;
@@ -40,7 +41,7 @@ public class Message {
 	private Meeting meeting;
 	
 	//index
-	@OneToOne
+	@OneToMany
 	@JoinColumn(name = "index_id")
 	private Index index;
 	
